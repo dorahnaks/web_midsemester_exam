794666d1aad1f3600ed18568cdf83af460eb5c44
Dorothy <dorahnakslove@gmail.com>
2025-03-28 10:56:39 +0300

    web midsemester exam

--- section_c/qn2.js
+++ section_c/qn2.js
@@ -1,4 +1,9 @@
 function sum_of_even_numbers(){
     let sum = 0;
-    
+    for (const num of arguments) {
+        if (num % 2 === 0) {
+            sum += num;
+        }
+    }
+    return sum;
 }
